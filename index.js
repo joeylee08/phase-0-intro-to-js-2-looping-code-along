67d242aa036718bbd9afef34ee2384092035bfba
Joseph <josephylee72@gmail.com>
2023-08-07 15:40:17 -0700

    completed looping lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solutions in this file
+function writeCards(array, event) {
+  let result = [];
+  for (let i = 0; i < array.length; i++) {
+    result.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+  }
+  return result;
+}
+
+console.log(writeCards(['Charlie', 'Samip', 'Ali'], 'birthday'))
+
+const gifts = ['teddy bear', 'drone', 'doll'];
+
+function wrapGifts(gifts) {
+  let i = 0;
+  while (i < gifts.length) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    i++;
+  }
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+function countDown(num) {
+  while (num > -1) {
+    console.log(num);
+    num--;
+  }
+  return;
+}
